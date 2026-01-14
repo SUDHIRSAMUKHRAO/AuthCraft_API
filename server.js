1cbc0ac13f484829c6b25b0c473d6f6df65940c7
SUDHIRSAMUKHRAO <samukhraosudhir33@gmail.com>
2026-01-14 15:52:14 +0530

    server file fixed

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const app = require("./src/app");
-require("dotenv").config();
- const connectDB = require("./src/config/db");
+const connectDB = require("./src/config/db");
+
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+module.exports = app;
